ccef12e7607afca3e80d21bb90067021c374561d
YxngXi1 <xuetianyang@gmail.com>
2024-08-25 11:16:53 +0800

    added auth and pushing onto db

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -12,5 +12,12 @@ export const authOptions: NextAuthOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
-    ]
+    ],
+    callbacks: {
+        async signIn({user, account}) {
+            console.log('User: ', user);
+            console.log('Account', account);
+            return user;
+        }
+    }
 }
